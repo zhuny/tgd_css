a9fa59f91f670de3476e2e27f732a3919f9942f4
Jihun Yang <zhuny@kaist.ac.kr>
2019-05-31 22:56:43 +0900

    폴더 이름 받기

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,41 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-module.exports = {
-	mode: "production",
-	entry: "./my.less",
-	output: {
-		path: path.resolve(__dirname, './dist'),
-		filename: "no-use.js"
-	},
-	module: {
-		rules: [{
-			test: /\.less$/,
-			use: [
-				MiniCssExtractPlugin.loader,
-				"css-loader",
-				"less-loader"
-			]
-		}]
-	},
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: '[name]-min.css'
-		})
-	],
-	optimization: {
-		minimizer: [new OptimizeCSSAssetsPlugin({})]
-	}
+module.exports = (env, argv) => {
+  if(!argv.name)
+    throw "Set Folder Name: npm run less -- --name={name}";
+
+  function abspath(relpath) {
+    return path.resolve(__dirname, './'+argv.name+'/'+relpath);
+  }
+
+  return {
+    mode: "production",
+    entry: {
+      mobile: abspath('custom_mobile.less'),
+      desktop: abspath('custom_desktop.less')
+    },
+    output: {
+      path: abspath('dist'),
+      filename: "no-use-[name].js"
+    },
+    module: {
+      rules: [{
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "less-loader"
+        ]
+      }]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name]-min.css'
+      })
+    ],
+    optimization: {
+      minimizer: [new OptimizeCSSAssetsPlugin({})]
+    }
+  }
 };
